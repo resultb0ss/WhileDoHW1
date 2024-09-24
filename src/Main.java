950a37da1870911c1317be09a2b23d9dbfd4c855
Admin <reteamboss@gmail.com>
2024-09-24 14:08:35 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /**
+         * Задание 1
+         */
+
+        int num = 0;
+        while(num <= 50){
+            if (num % 3 == 0){
+                System.out.println(num);
+            }
+            num++;
+        }
+        /**
+         * Задание 2
+         */
+
+        int result = 0;
+        for(int numTwo = 0;numTwo <= 100; numTwo++)
+        {
+            result = result + numTwo;
+            System.out.println(result);
+        }
+
     }
 }
